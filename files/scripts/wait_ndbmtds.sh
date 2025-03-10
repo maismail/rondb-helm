c161022a9cd0a2c694d29985c88baeb96d260873
Mahmoud Ismail <mahmoud@hopsworks.ai>
2025-03-10 09:55:08 +0100

    [HWORKS-2067] MySQL server should wait for all rondb datanodes before starting (#37)
    * [HWORKS-2067] MySQL server should wait for all rondb datanodes before starting
    
    * increase deploy stability timeout to 12 mins for rolling restarts
    
    * print status
    
    * increase timeout for global replication lifecycly test

--- files/scripts/wait_ndbmtds.sh
+++ files/scripts/wait_ndbmtds.sh
@@ -7,10 +7,49 @@ until nslookup $MGMD_HOSTNAME; do
     sleep $(((RANDOM % 2) + 2))
 done
 
-# Wait until data node is ready
-until ./docker/rondb_standalone/healthcheck.sh $MGMD_HOSTNAME:1186 1; do
-    echo "Dependency healthcheck of ndbmtd failed. Retrying in a bit"
+echo "trying to connect to the managemnt node.."
+until /srv/hops/mysql/bin/ndb_mgm --ndb-connectstring $MGMD_HOSTNAME -e "show"; do
+    echo "Waiting for $MGMD_HOSTNAME to be ready..."
     sleep $(((RANDOM % 2) + 2))
 done
+
+set -e
+
+# check all the ndbmtds in reverse order since stateful sets typically roll restart in the reverse order
+echo "Waiting for all ndbmtds to be ready..."
+for i in $(seq 0 5);
+do
+    echo "waiting for all ndbmtds retry $(( i + 1 )) out 6 retires"
+    /srv/hops/mysql/bin/ndb_waiter -c $MGMD_HOSTNAME  --timeout=$(( 120 * (2 ** i) ))
+    sleep $((2 ** i))
+done
+
+echo "Make sure that all ndbmtds are ready..."
+
+{{- $nodeIds := list -}}
+{{- range $nodeGroup := until ($.Values.clusterSize.numNodeGroups | int) -}}
+{{- range $replica := until 3 -}}
+{{- $isActive := 0 -}}
+{{- if lt $replica ($.Values.clusterSize.activeDataReplicas | int) -}}
+  {{- $isActive = 1 -}}
+{{- end -}}
+{{- $offset := ( mul $nodeGroup 3) -}}
+{{- $nodeId := ( add $offset (add $replica 1)) -}}
+{{- if eq $isActive 1 -}}
+{{- $nodeIds = append $nodeIds $nodeId -}}
+{{- end -}}
+{{- end -}}
+{{- end -}}
+
+{{ range $nodeId := reverse $nodeIds }}
+echo "Check if ndbmtd {{ $nodeId }} is ready"
+STATUS=$(/srv/hops/mysql/bin/ndb_mgm --ndb-connectstring $MGMD_HOSTNAME -e "{{ $nodeId }} status")
+echo $STATUS
+if [[ "$STATUS" != *"started"* ]]; then
+    echo "Node {{ $nodeId }} is not ready, exiting..."
+    exit 1
+fi
+sleep $(((RANDOM % 2) + 2))
+{{ end }}
 
 echo "Successfully waited for an ndbmtd to be ready"
