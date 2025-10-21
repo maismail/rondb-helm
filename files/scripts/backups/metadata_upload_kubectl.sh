5af43f717fa96c41a01f0fa044bac9e888812c7b
Mahmoud Ismail <mahmoud@hopsworks.ai>
2025-10-21 11:50:59 +0200

    [CLOUD-785] Refactor backups to use global config if not local config defined

--- files/scripts/backups/metadata_upload_kubectl.sh
+++ files/scripts/backups/metadata_upload_kubectl.sh
@@ -22,7 +22,8 @@ kubectl cp \
 ls -la $LOCAL_BACKUP_DIR
 
 {{ include "rondb.backups.defineJobNumberEnv" $ }}
-REMOTE_BACKUP_DIR={{ include "rondb.rcloneBackupRemoteName" . }}:{{ .Values.backups.s3.bucketName }}/{{ include "rondb.takeBackupPathPrefix" . }}/$JOB_NUMBER
+REMOTE_BACKUP_DIR={{ include "rondb.rcloneBackupRemoteName" . }}:{{include "rondb.backups.bucketName" (dict "backupConfig" .Values.backups "global" .Values.global)}}/{{ include "rondb.takeBackupPathPrefix" . }}/$JOB_NUMBER
+echo && rclone mkdir $REMOTE_BACKUP_DIR
 echo && rclone ls $REMOTE_BACKUP_DIR
 echo "Copying backup from $LOCAL_BACKUP_DIR to $REMOTE_BACKUP_DIR"
 rclone move $LOCAL_BACKUP_DIR $REMOTE_BACKUP_DIR
